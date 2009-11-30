c392f6becfa3aeef1b90c2056205caa0df128cad
James <james@frantech.ca>
2009-11-30 09:14:03 +0000

    Added version.pm for version tracking, and CGIErrors.pm. CGIErrors is simply use'd and it overrides the default DIE and WARN handlers to output snazzy pages. No more 500!

---
+++ Azusa/CGIErrors.pm
@@ -0,0 +1,62 @@
+#!/usr/bin/perl -w
+use strict;
+
+BEGIN {
+	use Azusa::version;
+	my $azusa_version = Azusa::version::version();
+       	$SIG{__DIE__}  = \&handleErrors;
+       	$SIG{__WARN__} = \&handleWarnings;
+       	sub handleErrors {
+               	my ($error) = @_;
+               	my ($module, $routine) = (caller(1))[0,3];
+               	$module  = 'main' if (!$module);
+               	$routine = 'main' if (!$routine);
+               	my $traceback;
+               	my $y;
+               	for ($y = 1; caller($y); $y++) {
+                       	# do nothing.
+               	}
+               	$y--;
+               	my $z = 0;
+               	$traceback = "Caller traceback:\n";
+               	my $sandbox;
+               	for (my $x = $y; caller($x); $x--, $z++) {
+                       	my $sub  = (caller($x))[3];
+                       	my $addr = \&$sub;
+                       	$addr    =~ s/(CODE\(|\))//g;
+                       	$sub     =~ s/\(eval\)/\[sandbox create\]/g;
+                       	$sandbox = 1 if ($sub =~ /sandbox create/);
+                       	$traceback .= (('&nbsp;' x 2) x $z).($z ? '\_ ' : ' |-').$addr.': <strong>'.$sub.'();</strong> at line '.(caller($x))[2]."\n";
+               	}
+               	print <<EOF;
+Content-type: text/html
+
+<html>
+ <head>
+   <title>Error</title>
+   <style>
+body {
+  font-family: monospace;
+}
+h1 {
+  text-align: center;
+  color: #FF0000;
+}
+#trace {
+  color: #FF0000;
+}
+  </style>
+ </head>
+ <body>
+  <h1>Exception raised</h1>Subroutine <strong>$routine</strong> within module <strong>$module</strong> raised exception: <strong>$error</strong><br /><hr />
+  <div class="trace"><pre>$traceback</pre></div>
+  <hr />
+  <address>powered by libAzusa version $azusa_version</address>
+ </body>
+</html>
+EOF
+               	exit(1) if (!$sandbox);
+       	}
+};
+
+1;
---
+++ Azusa/version.pm
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+use strict;
+package Azusa::version;
+
+sub version { '0.2'; }
+
+1;
